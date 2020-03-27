62b961926145d8bcc35c2e79fd0aaf0bf150856d
shannonli7 <lishann@umich.edu>
2020-03-27 14:58:10 -0400

    beginning works of spotify api

--- spotify.py
+++ spotify.py
@@ -1,1 +1,14 @@
-print("testing github")
+import spotipy
+from spotipy.oauth2 import SpotifyClientCredentials
+
+lz_uri = 'spotify:artist:3mIj9lX2MWuHmhNCA7LSCW'
+
+#set SPOTIPY_CLIENT_ID and SPOTIPY_CLIENT_SECRET
+spotify = spotipy.Spotify(client_credentials_manager=SpotifyClientCredentials("17f86f3cbfd64d5e8d650c809760fee0", "315a2c5b3f924ef6bf86af634ff4dce1"))
+results = spotify.artist_top_tracks(lz_uri)
+
+for track in results['tracks'][:10]:
+    print('track    : ' + track['name'])
+    #print('audio    : ' + track['preview_url'])
+    #print('cover art: ' + track['album']['images'][0]['url'])
+    #print()
