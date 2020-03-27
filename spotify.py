98e100ce7e2f84a710c07d9fd71cf55be6d5c491
shannonli7 <lishann@umich.edu>
2020-03-27 15:17:15 -0400

    added follower function

--- spotify.py
+++ spotify.py
@@ -7,7 +7,11 @@ artist1_uri = 'spotify:artist:3mIj9lX2MWuHmhNCA7LSCW'
 spotify = spotipy.Spotify(client_credentials_manager=SpotifyClientCredentials("17f86f3cbfd64d5e8d650c809760fee0", "315a2c5b3f924ef6bf86af634ff4dce1"))
 #predefined functions of spotify's api
 results = spotify.artist_top_tracks(artist1_uri)
-
+#result = spotify.audio_analysis("spotify:track:5hc71nKsUgtwQ3z52KEKQk")
+#print(result)
+#total number of followers given
+#result = spotify.search(q="The 1975", limit = 2, offset=0, type="artist")
+#print(result)
 def get_tracks():
     for track in results['tracks'][:10]:
         print('track    : ' + track['name'])
