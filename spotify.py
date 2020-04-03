4fe36d9c9852343dcf9ae677f1caf1ade7c6ee22
shannonli7 <lishann@umich.edu>
2020-04-03 09:48:07 -0400

    some changes to spotify.py

--- spotify.py
+++ spotify.py
@@ -1,19 +1,26 @@
 import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
 
+#each of the artist's unique uri from Spotify app
 artist1_uri = 'spotify:artist:3mIj9lX2MWuHmhNCA7LSCW'
 
 #set SPOTIPY_CLIENT_ID and SPOTIPY_CLIENT_SECRET
 spotify = spotipy.Spotify(client_credentials_manager=SpotifyClientCredentials("17f86f3cbfd64d5e8d650c809760fee0", "315a2c5b3f924ef6bf86af634ff4dce1"))
 #predefined functions of spotify's api
-results = spotify.artist_top_tracks(artist1_uri)
+#audio_analysis
 #result = spotify.audio_analysis("spotify:track:5hc71nKsUgtwQ3z52KEKQk")
-#print(result)
+#print(results)s
+def get_tracks(artist_uri):
+    dictionary = {}
+    lst_of_tracks = []
+    results = spotify.artist_top_tracks(artist_uri)
+    for track in results['tracks'][:10]:
+        lst_of_tracks.append(track["name"])
+    artist_name = results["tracks"][0]["album"]["artists"][0]["name"]
+    dictionary[artist_name] = lst_of_tracks
+    return dictionary
 #total number of followers given
 #result = spotify.search(q="The 1975", limit = 2, offset=0, type="artist")
-#print(result)
-def get_tracks():
-    for track in results['tracks'][:10]:
-        print('track    : ' + track['name'])
-        #print('audio    : ' + track['preview_url'])
-        #print('cover art: ' + track['album']['images'][0]['url'])
+#def get_followers():
+
+print(get_tracks(artist1_uri))
