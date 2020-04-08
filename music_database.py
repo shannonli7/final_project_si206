26cfccf95bdfb108a5d0068835bf0de282981d5d
shannonli7 <lishann@umich.edu>
2020-04-08 13:27:06 -0400

    some database inputted- need to clarify the 20 limits

--- music_database.py
+++ music_database.py
@@ -5,7 +5,7 @@ import os
 
 def setupdatabase(database_name):
     path = os.path.dirname(os.path.abspath(__file__))
-    conn = sqlite3.connect(path+'/'+db_name)
+    conn = sqlite3.connect(path+'/'+database_name)
     cur = conn.cursor()
     return cur, conn
 
@@ -14,7 +14,7 @@ def create_artist_table(cur, conn):
     artist_uri = ["spotify:artist:3mIj9lX2MWuHmhNCA7LSCW", "spotify:artist:3IunaFjvNKj98JW89JYv9u", 
     "spotify:artist:2eam0iDomRHGBypaDQLwWI", "spotify:artist:5JZ7CnR6gTvEMKX4g70Amv", "spotify:artist:536BYVgOnRky0xjsPT96zl", 
     "spotify:artist:4Uc8Dsxct0oMqx0P6i60ea", "spotify:artist:0Ya43ZKWHTKkAbkoJJkwIB", "spotify:artist:6d4jrmreCmsenscuieJERc", 
-    "0szWPxzzE8DVEfXFRCLBUb", "spotify:artist:163tK9Wjr9P9DmM0AVK7lm"]
+    "spotify:artist:0szWPxzzE8DVEfXFRCLBUb", "spotify:artist:163tK9Wjr9P9DmM0AVK7lm"]
 
     artists = ["The 1975", "The Japanese House", "Bleachers", "Lauv", 
     "Two Door Cinema Club", "Conan Gray", "The Wombats", "The Band CAMINO", 
@@ -26,52 +26,59 @@ def create_artist_table(cur, conn):
         cur.execute("INSERT INTO Artists (artist_uri,artist_name) VALUES (?,?)",(artist_uri[i],artists[i]))
     conn.commit()
 
-def create_followers_table(cur, conn):
+def create_followers_table(filename, cur, conn):
+    filobj = open(filename)
+    filedata = filobj.read()
+    filobj.close()
+    json_data = json.loads(filedata)
 
-    artist_uri = ["spotify:artist:3mIj9lX2MWuHmhNCA7LSCW", "spotify:artist:3IunaFjvNKj98JW89JYv9u", 
-    "spotify:artist:2eam0iDomRHGBypaDQLwWI", "spotify:artist:5JZ7CnR6gTvEMKX4g70Amv", "spotify:artist:536BYVgOnRky0xjsPT96zl", 
-    "spotify:artist:4Uc8Dsxct0oMqx0P6i60ea", "spotify:artist:0Ya43ZKWHTKkAbkoJJkwIB", "spotify:artist:6d4jrmreCmsenscuieJERc", 
-    "0szWPxzzE8DVEfXFRCLBUb", "spotify:artist:163tK9Wjr9P9DmM0AVK7lm"]
-
-    artists = ["The 1975", "The Japanese House", "Bleachers", "Lauv", 
-    "Two Door Cinema Club", "Conan Gray", "The Wombats", "The Band CAMINO", 
-    "Flor", "Lorde"]
-
-    lst_followers = [3922606, 354715, 364344, 2589051, 1833816, 951347, 761801, 151465, 3648080, 5626478]
     cur.execute("DROP TABLE IF EXISTS Followers")
     cur.execute("CREATE TABLE Followers (artist_uri TEXT PRIMARY KEY, artist_name TEXT, followers INTEGER)")
-    for i in range(10):
-        cur.execute("INSERT INTO Followers (artist_uri,artist_name, followers) VALUES (?,?,?)",(artist_uri[i],artists[i], lst_followers[i]))
+    for dictionary in json_data:
+        artist_uri = dictionary ["artist_uri"]
+        artist_name = dictionary ["name"]
+        followers = dictionary ["followers"]
+        cur.execute("INSERT INTO Followers (artist_uri,artist_name, followers) VALUES (?,?,?)",(artist_uri,artist_name, followers))
     conn.commit()
 
-def create_track_price_table(cur, conn):
-    artists = ["The 1975", "The Japanese House", "Bleachers", "Lauv", 
-    "Two Door Cinema Club", "Conan Gray", "The Wombats", "The Band CAMINO", 
-    "Flor", "Lorde"]
+def create_track_price_table(filename, cur, conn):
+    filobj = open(filename)
+    filedata = filobj.read()
+    filobj.close()
+    json_data = json.loads(filedata)
+    cur.execute("DROP TABLE IF EXISTS trackprice")
+    cur.execute("CREATE TABLE trackprice (artist_name TEXT, track_name TEXT, track_price INTEGER)")
 
-    track_prices1 = [1.29, 1.29, 1.29, 1.29, 1.29, 1.29, 1.99, 1.29, 1.99, 1.29]
-    track_prices2 = [1.29, 1.29, 1.29, 1.29, 1.99, 1.29, 1.29, 1.99, 1.29, 1.29]
-    track_prices3 = [1.29, 1.29, 1.99, 1.29, 1.00, 1.29, 1.29, 1.99, 1.29, 1.29]
-    track_prices4 = [1.00, 1.29, 1.99, 1.29, 0.99, 0.99, 1.29, 1.99, 1.29, 1.29]
-    track_prices5 = [1.29, 1.29, 1.29, 1.99, 1.29, 1.29, 1.00, 1.29, 1.29, 1.29]
-    track_prices6 = [1.29, 1.99, 1.00, 1.29, 1.29, 1.99, 1.29, 1.29, 0.69, 1.29]
-    track_prices7 = [1.29, 1.29, 1.29, 1.29, 1.29, 1.29, 1.29, 1.29, 1.29, 1.29]
-    track_prices8 = [1.29, 1.29, 1.29, 0.99, 1.29, 1.29, 1.29, 1.29, 1.29, 1.29]
-    track_prices9 = [0.99, 1.29, 0.69, 1.29, 0.99, 0.99, 0.99, 0.99, 0.99, 0.99]
-    track_prices10 = [1.29, 1.29, 1.00, 1.29, 1.29, 1.29, 1.29, 1.29, 1.29, 1.29]
+    for dictionary in json_data:
+        artist_name = dictionary ["name"]
+        track_name = dictionary ["tracks"]
+        track_price = dictionary ["trackprice"]
+        for i in range(10):
+            cur.execute("INSERT INTO trackprice (artist_name, track_name, track_price) VALUES (?,?,?)",(artist_name, track_name[i], track_price[i]))
+    conn.commit()
 
-    tracks1 = ['Somebody Else', "It's Not Living (If It's Not With You)", 'Me & You Together Song', 'Love It If We Made It', 'The Birthday Party', 'Be My Mistake', 'TOOTIMETOOTIMETOOTIME', 'Sincerity Is Scary', 'Narcissist (feat. The 1975)', 'I Always Wanna Die (Sometimes)']
-    tracks2 = ['Saw You In A Dream', 'Chewing Cotton Wool', 'Something Has to Change', "Maybe You're the Reason", 'Lilo', 'You Seemed so Happy', 'i saw you in a dream', 'Cool Blue', 'Landslide - Recorded At Spotify Studios NYC', 'Still']
-    tracks3 = ['Rollercoaster', 'I Wanna Get Better', "Don't Take The Money", 'Goodmorning', 'Wake Me', "Let's Get Married", 'Like a River Runs', 'Wild Heart', 'I Miss Those Days', 'Entropy']
-    tracks4 = 'I Like Me Better', 'Who (feat. BTS)', "i'm so tired...", "fuck, i'm lonely (with Anne-Marie)", 'Modern Loneliness', 'Mean It', 'Invisible Things', 'Make It Right (feat. Lauv)', 'Tattoos Together', 'Feelings']
-    tracks5 = ['What You Know', 'Undercover Martyn', 'Something Good Can Work', 'Sun', 'Changing of the Seasons', 'I Can Talk', 'Are We Ready? (Wreck)', 'Sleep Alone', 'Talk - Single Edit', 'Next Year']
-    tracks6 = ['Maniac', 'Crush Culture', 'Generation Why', 'The Story', 'Lookalike', 'Wish You Were Sober', 'Heather', 'Idle Town', 'The King', 'Comfort Crowd']
-    tracks7 = ["Let's Dance to Joy Division", 'Kill the Director', 'Moving to New York', 'Turn', 'Tokyo - Vampires & Wolves', 'Cheetah Tongue', 'Lemon to a Knife Fight', 'Greek Tragedy', 'Pink Lemonade', 'Lethal Combination']
-    tracks8 = ['2 / 14', 'Less Than I Do', 'See Through', 'Daphne Blue', 'My Thoughts On You', 'The Black and White', 'Know Me', 'Berenstein', 'Fool of Myself', 'Hush Hush']
-    tracks9 = ['dancing around', 'back again', 'hold on', 'yellow (feat. MisterWives)', 'slow motion', 'warm blood', 'where do you go', 'heart', 'rely', 'slow motion']
-    tracks9 = ['Royals', 'Team', 'Ribs', 'Green Light', 'Liability', 'Tennis Court', 'Supercut', 'Perfect Places', 'Homemade Dynamite - REMIX', 'The Love Club']
+def create_recommendations_table(filename, cur, conn):
+    filobj = open(filename)
+    filedata = filobj.read()
+    filobj.close()
+    json_data = json.loads(filedata)
+    cur.execute("DROP TABLE IF EXISTS Recommendations")
+    cur.execute("CREATE TABLE Recommendations (artist_name TEXT, recommended_artist TEXT)")
 
-def create_recommendations_table(cur, conn):
-    artists = ["The 1975", "The Japanese House", "Bleachers", "Lauv", 
-    "Two Door Cinema Club", "Conan Gray", "The Wombats", "The Band CAMINO", 
-    "Flor", "Lorde"]
+    for dictionary in json_data:
+        artist_name = dictionary ["name"]
+        recommended_artists = dictionary ["recommendations"]
+        for i in range(10):
+            cur.execute("INSERT INTO Recommendations (artist_name, recommended_artist) VALUES (?,?)",(artist_name,recommended_artists[i]))
+ 
+    conn.commit()
+
+def main():
+    cur, conn = setupdatabase("music_database.db")
+    create_artist_table(cur, conn)
+    create_followers_table("data.json", cur, conn)
+    create_track_price_table("data.json", cur, conn)
+    create_recommendations_table("data.json", cur, conn)
+
+if __name__ == "__main__":
+    main()
--- taste_dive.py
+++ taste_dive.py
@@ -2,7 +2,7 @@ import json
 import requests
 
 API_KEY = "361067-SI206Pro-349C755Q"
-def get_songs_from_tastedive(music):
+def get_dictionary_from_tastedive(music):
     baseurl = "https://tastedive.com/api/similar"
     paramsd = {}
     paramsd["k"] = API_KEY
@@ -14,9 +14,34 @@ def get_songs_from_tastedive(music):
     reqs = requests.get(baseurl, paramsd)
 
     return reqs.json()
-#testing
-#nested dictionary of related artists printed!
-print(get_songs_from_tastedive("The 1975"))
 
-#function to make a get related artist and put to a list
-#function for the dictionary of the related artist (key) and the youtube link (value)
+def get_related_artist(artist_name):
+    results = get_dictionary_from_tastedive(artist_name)
+    lst_of_recommendations = []
+    for dictionary in list(results["Similar"]["Results"]):
+        lst_of_recommendations.append(dictionary["Name"])
+    return lst_of_recommendations
+
+print(get_related_artist("The 1975"))
+print(get_related_artist("The Japanese House"))
+print(get_related_artist("Bleachers"))
+print(get_related_artist("Lauv"))
+print(get_related_artist("Two Door Cinema Club"))
+print(get_related_artist("Conan Gray"))
+print(get_related_artist("The Wombats"))
+print(get_related_artist("The Band CAMINO"))
+print(get_related_artist("flor"))
+print(get_related_artist("Lorde"))
+
+#returned recommendations
+recommended_artists1  = ['The Neighbourhood', 'Catfish And The Bottlemen', 'Coasts', 'The Neighborhood', 'Lewis Watson', 'Swim Deep', 'Lany', 'Bad Suns', 'Kodaline', 'Saint Raymond']
+recommended_artists2 = ['Phoebe Bridgers', 'Day Wave', 'Pale Waves', 'Boy Pablo', 'Varsity', 'Lany', 'Gus Dapperton', 'Wallows', 'Dandelion Hands', 'Girl In Red']
+recommended_artists3 = ['Hunter Hunted', 'The Griswolds', 'The Colourist', 'Magic Man', "The Mowgli's", 'Ghost Beach', 'Wild Cub', 'Young Rising Sons', 'Smallpools', 'Autoheart']
+recommended_artists4 = ['Quinn Xcii', 'Lany', "Olivia O'brien", 'Yungblud', 'Astrid S', 'Gnash', 'Dean Lewis', 'Wrabel', 'Syml', 'Sasha Sloan']
+recommended_artists5 = ['Bombay Bicycle Club', 'Darwin Deez', 'The Wombats', 'The Temper Trap', 'The Naked And Famous', 'Friendly Fires', 'The Vaccines', 'The Drums', 'San Cisco', 'Miike Snow']
+recommended_artists6 = ['Mxmtoon', 'Sasha Sloan', 'Cavetown', 'Declan Mckenna', 'Girl In Red', 'Fur', 'Wallows', 'Clairo', "Why Don't We", 'Simon Vs. The Homo Sapiens Agenda']
+recommended_artists7 = ['The Pigeon Detectives', 'The Holloways', 'The Hoosiers', 'The Courteeners', 'The Vaccines', 'Milburn', 'The View', 'Jack Peñate', 'The Enemy', 'The Maccabees']
+recommended_artists8 = ['Hayley Kiyoko', 'The Wldlfe', 'Flor', 'Troye Sivan', 'Joan', 'Paramore', 'The Truman Show', 'COIN', 'Smallpools', 'Bad Suns']
+recommended_artists9 = ['Wild Party', 'The Academic', 'Coast Modern', 'Max Frost', 'Grizfolk', 'Cruisr', 'Coin', 'Rina Sawayama', 'Young Rising Sons', 'S Club 7']
+recommended_artists10 = ['Charli Xcx', 'Icona Pop', 'Foxes', 'Tove Lo', 'Haim', 'Sky Ferreira', 'Broods', 'Florence Welch', 'Echosmith', 'Ms Mr']
+
